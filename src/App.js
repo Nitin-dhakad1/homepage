21eae890ca0dc63b817e47b5c8cfc093b46db05d
Nitin <unlikenitin@gmail.com>
2023-04-09 02:32:51 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as Logo } from "./logo.svg";
+import styles from './App.css'
+import icon from './image-removebg-preview.png'
+import prog from './image__1_-removebg-preview.png'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='navigation-bar'>
+        <Logo/>
+        <button className='navigation-button1'>Categories</button>
+        <div className='bar1'></div>
+        <button className='navigation-button2'>Community</button>
+        <div className='bar2'></div>
+        <button className='navigation-button3'>Create</button>
+        <div className='bar3'></div>
+        <button className='navigation-button4'>About us</button>
+        <img src={icon} className="icon"></img>
+      </div>
+      <div className='screen'>
+        <p className='para'>Work in progress!
+This website is still under progress. Come back later for more features. For any queries, please contact with the above</p>
+        <img src={prog} className="prog"></img>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
